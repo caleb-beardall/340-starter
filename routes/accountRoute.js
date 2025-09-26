417e204bac97046b07e73461880ec3b843e04cbc
caleb-beardall <calebbeardall@byui.edu>
2025-09-25 22:10:25 -0600

    Completed Week 4's Learning Activities

---
+++ routes/accountRoute.js
@@ -0,0 +1,22 @@
+// Needed Resources
+const express = require("express")
+const router = new express.Router()
+const accController = require("../controllers/accountController")
+const utilities = require("../utilities")
+const regValidate = require("../utilities/account-validation")
+
+// Route to build login view
+router.get("/login", utilities.handleErrors(accController.buildLogin));
+
+// Route to build registration view
+router.get("/register", utilities.handleErrors(accController.buildRegister))
+
+// Handle POST requests to register a new user account
+router.post(
+    "/register",
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accController.registerAccount)
+)
+
+module.exports = router;
